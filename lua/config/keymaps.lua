1177e6b2b15eb2b92f5534fd224d1ce760f7ad87
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-01-23 12:29:14 +0100

    neotree and buffer manager improvements

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,13 +4,58 @@
 
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 
+-- buffer manager ---------------
+
 -- Keybinding for Buffer Manager
 -- https://github.com/j-morano/buffer_manager.nvim
-vim.keymap.set("n", "<S-Tab>", ":lua require('buffer_manager.ui').toggle_quick_menu()<CR>", {
-    desc = "Toggle Buffer Manager",
+-- vim.keymap.set("n", "<C-Tab>", ":lua require('buffer_manager.ui').toggle_quick_menu()<CR>", {
+--     desc = "Toggle Buffer Manager",
+--     noremap = true,
+--     silent = true,
+-- })
+
+local buffer_manager = require("buffer_manager.ui")
+
+-- State tracking for menu
+local menu_open = false
+
+-- Open or toggle the menu
+vim.keymap.set("n", "<C-Tab>", function()
+    if not menu_open then
+        buffer_manager.toggle_quick_menu()
+        menu_open = true
+    else
+        -- Navigate down in the menu
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Down>", true, false, true), "n", true)
+    end
+end, {
+    desc = "Open/Toggle Buffer Manager or Move Down",
     noremap = true,
     silent = true,
 })
+
+-- Navigate up in the menu with <C-S-Tab>
+vim.keymap.set("n", "<C-S-Tab>", function()
+    if menu_open then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Up>", true, false, true), "n", true)
+    end
+end, {
+    desc = "Move Up in Buffer Manager Menu",
+    noremap = true,
+    silent = true,
+})
+
+-- Hook into the buffer selection (`<CR>`) to update menu state
+vim.api.nvim_create_autocmd("BufWinLeave", {
+    pattern = "*",
+    callback = function()
+        if menu_open then
+            menu_open = false -- Close the menu state when leaving the buffer menu
+        end
+    end,
+})
+
+-- buffer manager ---------------
 
 -- vim.keymap.set("n", "<A-s>", ":wa<CR>", { desc = "Save all buffers (Alt-S)" })
 -- vim.keymap.set("i", "<A-s>", "<Esc>:wa<CR>i", { desc = "Save all buffers and return to insert mode (Alt-S)" })
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,88 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        filesystem = {
+            -- follow_current_file = true, -- Optional: automatically focus on the current file
+            -- group_empty_dirs = false,   -- Optional: group empty directories
+            mappings = {
+                -- ["z"] = "close_all_nodes",  -- Close all nodes with `z`
+                ["Z"] = "expand_all_nodes", -- Expand all nodes with `Z`
+            },
+
+            renderers = {
+                directory = {
+                    { "indent" },
+                    { "icon" },
+                    { "current_filter" },
+                    {
+                        "container",
+                        content = {
+                            { "name", zindex = 10 },
+                            {
+                                "symlink_target",
+                                zindex = 10,
+                                highlight = "NeoTreeSymbolicLinkTarget",
+                            },
+                            { "clipboard", zindex = 10 },
+                            {
+                                "diagnostics",
+                                errors_only = true,
+                                zindex = 20,
+                                align = "right",
+                                hide_when_expanded = true,
+                            },
+                            { "git_status", zindex = 10, align = "right", hide_when_expanded = true },
+                            -- { "file_size", zindex = 10, align = "right" },
+                            -- { "type", zindex = 10, align = "right" },
+                            -- { "last_modified", zindex = 10, align = "right" },
+                            -- { "created", zindex = 10, align = "right" },
+                        },
+                    },
+                },
+                file = {
+                    { "indent" },
+                    { "icon" },
+                    {
+                        "container",
+                        content = {
+                            {
+                                "name",
+                                zindex = 10,
+                            },
+                            {
+                                "symlink_target",
+                                zindex = 10,
+                                highlight = "NeoTreeSymbolicLinkTarget",
+                            },
+                            { "clipboard", zindex = 10 },
+                            { "bufnr", zindex = 10 },
+                            -- { "modified", zindex = 20, align = "right" },
+                            { "diagnostics", zindex = 20, align = "right" },
+                            { "git_status", zindex = 10, align = "right" },
+                            -- { "file_size", zindex = 10, align = "right" },
+                            -- { "type", zindex = 10, align = "right" },
+                            -- { "last_modified", zindex = 10, align = "right" },
+                            -- { "created", zindex = 10, align = "right" },
+                        },
+                    },
+                },
+            },
+        },
+        window = {
+            width = 80, -- Optional: Set Neo-tree window width
+            mappings = {
+                -- ["z"] = "close_all_nodes",  -- Close all nodes with `z`
+                ["Z"] = "expand_all_nodes", -- Expand all nodes with `Z`
+            },
+        },
+        -- event_handlers = {
+        --   -- Optional: Automatically close Neo-tree when opening a file
+        --   {
+        --     event = "file_opened",
+        --     handler = function(file_path)
+        --       require("neo-tree").close_all()
+        --     end,
+        --   },
+        -- },
+    },
+}
