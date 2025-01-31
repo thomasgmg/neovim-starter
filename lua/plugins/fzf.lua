55775c50069bb3504274615468ab53e5f587a40a
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-01-31 12:18:22 +0100

    commented code to remove lsp keymaps

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,25 +1,38 @@
 return {
-    "ibhagwan/fzf-lua",
+    {
+        "ibhagwan/fzf-lua",
 
-    opts = function(_, opts)
-        local file_win_opts = {
-            -- fullscreen = true,
-            preview = {
-                layout = "vertical",
-                vertical = "down:50%",
-            },
-        }
-        return {
-            defaults = {
-                -- formatter = "path.dirname_first",
-                formatter = "path.filename_first",
-            },
-            files = {
-                winopts = file_win_opts,
-                -- previewer = "false",
-                cmd = "find",
-                find_opts = [[-type f -not -path '*/\.git/*' -printf "%f\n"]],
-            },
-        }
-    end,
+        opts = function(_, opts)
+            local file_win_opts = {
+                -- fullscreen = true,
+                preview = {
+                    layout = "vertical",
+                    vertical = "down:50%",
+                },
+            }
+            return {
+                defaults = {
+                    -- formatter = "path.dirname_first",
+                    formatter = "path.filename_first",
+                },
+                files = {
+                    winopts = file_win_opts,
+                    -- previewer = "false",
+                    cmd = "find",
+                    find_opts = [[-type f -not -path '*/\.git/*' -printf "%f\n"]],
+                },
+            }
+        end,
+    },
+    -- uncomment to remove keys
+    -- {
+    --     "neovim/nvim-lspconfig",
+    --     opts = function(_, opts)
+    --         local Keys = require("lazyvim.plugins.lsp.keymaps").get()
+    --
+    --         vim.list_extend(Keys, {
+    --             { "gr", "<Nop>" },
+    --         })
+    --     end,
+    -- },
 }
