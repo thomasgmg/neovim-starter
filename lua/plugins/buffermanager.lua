e8b3cb505996a74340558bc5273d0c71555a505b
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-01-31 14:11:54 +0100

    improve buffer manager keymaps and logic

--- lua/plugins/buffermanager.lua
+++ lua/plugins/buffermanager.lua
@@ -1,12 +1,51 @@
 return {
     -- add gruvbox
     "j-morano/buffer_manager.nvim",
+
     config = function()
         require("buffer_manager").setup({
             order_buffers = "lastused",
             focus_alternate_buffer = true,
             loop_nav = true,
             short_file_names = true,
+
+            select_menu_item_commands = {
+                edit = {
+                    key = "l",
+                    command = "edit",
+                },
+            },
+        })
+
+        local buffer_manager = require("buffer_manager.ui")
+
+        vim.keymap.set("n", "<C-Tab>", function()
+            buffer_manager.toggle_quick_menu()
+        end, {
+            desc = "Open/Toggle Buffer Manager or Move Down",
+            noremap = true,
+            silent = true,
+        })
+
+        vim.keymap.set("n", "k", function()
+            if buffer_manager.is_menu_open and buffer_manager.is_menu_open() then
+                buffer_manager.nav_prev()
+            end
+        end, {
+            desc = "Move Up in Buffer Manager Menu",
+            noremap = true,
+            silent = true,
+        })
+
+        -- Navigate up in the menu with <C-S-Tab>
+        vim.keymap.set("n", "j", function()
+            if buffer_manager.is_menu_open and buffer_manager.is_menu_open() then
+                buffer_manager.nav_next()
+            end
+        end, {
+            desc = "Move Up in Buffer Manager Menu",
+            noremap = true,
+            silent = true,
         })
     end,
 }
