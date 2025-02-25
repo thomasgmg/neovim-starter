68e1aff5d426b4cff485be8cafb4beeb96a1a7dc
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-02-25 09:36:11 +0100

    delete uneeded

--- lua/plugins/replacewithregister.lua
+++
@@ -1,8 +0,0 @@
-if true then return {} end
-
-return {
-    "vim-scripts/ReplaceWithRegister",
-    config = function()
-		vim.api.nvim_set_keymap('n', '<leader>gr', '<Plug>ReplaceWithRegisterOperator', { noremap = false, silent = true })
-	end,
-}
