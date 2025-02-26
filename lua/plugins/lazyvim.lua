5416034d4edc9222904a6b3e4af6f80ffebb8d57
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-02-26 15:30:32 +0100

    add variable to swtich between local and repo colorscheme

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -1,25 +1,33 @@
--- https://grok.com/share/bGVnYWN5_6adaf8e3-9d1f-40d6-be44-dfef9d16100d
+local is_local = false
 
-return {
-    -- uncomment for testing local changes
-    -- {
-    --     dir = "~/projects/nvim/neodarcula.nvim", -- Path to your local plugin
-    --     -- name = "neodarcula",
-    --     lazy = false, -- Load immediately for testing
-    --     priority = 1000, -- Ensure it loads early
-    --
-    --     opts = {
-    --         transparent = true,
-    --     },
-    -- },
-    {
+local colorscheme_plugin_table
+
+if is_local then
+    colorscheme_plugin_table = {
+        dir = "~/projects/nvim/neodarcula.nvim", -- Path to your local plugin
+        -- name = "neodarcula",
+        lazy = false, -- Load immediately for testing
+        priority = 1000, -- Ensure it loads early
+
+        opts = {
+            transparent = true,
+            dim = true,
+        },
+    }
+else
+    colorscheme_plugin_table = {
         "pmouraguedes/neodarcula.nvim",
         lazy = false,
         priority = 1000,
         opts = {
             transparent = true,
+            dim = true,
         },
-    },
+    }
+end
+
+return {
+    colorscheme_plugin_table,
     {
         "LazyVim/LazyVim",
         opts = {
